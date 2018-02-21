f280cab3ccafa6880841aa22e2935e845020cdcc
Hamirall <francois.gazaube@imie.fr>
2018-02-21 11:11:48 +0100

    connexion.php fait

---
+++ connexion.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = '127.0.0.1'; 
+$port = '3306';
+$db = 'vote_bdd';
+$login = 'root';
+$password = '';
+
+try {
+    
+    $conn = new PDO("mysql:host=$host", $login, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE $db";
+    $conn->exec($sql);
+    echo "BDD ready";
+
+
+} catch (PDOException $e) {
+    
+    var_dump($e->getMessage()); 
+
+} 
+
