7c2f4a88240cf6fa50468b15ab14094fe7143e6d
Eleonore <eleonore.dehombreux@imie.fr>
2018-02-22 11:16:17 +0100

    connexion function

--- Query/connexion.php
+++ Query/connexion.php
@@ -1,18 +1,25 @@
 <?php
 
-$host = '127.0.0.1'; 
-$port = '3306';
-$db = 'vote_bdd';
-$login = 'root';
-$password = '';
+function connectDb()
+{
+    $host = '127.0.0.1';
+    $port = '3306';
+    $db = 'vote_bdd';
+    $login = 'root';
+    $password = '';
 
-try {
-    
-    $conn = new PDO('mysql:host='.$host.';port='.$port. ';dbname='.$db, $login, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {
 
-} catch (PDOException $e) {
-    
-    var_dump($e->getMessage()); 
+        $conn = new PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $db, $login, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-} 
+    } catch (PDOException $e) {
+
+        var_dump($e->getMessage());
+
+    }
+
+    echo 'connected';
+    return $conn;
+}
+
--- Query/person.php
+++ Query/person.php
@@ -2,53 +2,66 @@
 include 'connexion.php';
 
 //Créer une personne
-function createPerson(&$conn, $email, $password, $firstname, $lastname)
+function createPerson($email, $password, $firstname, $lastname)
 {
-    $conn->exec("INSERT INTO person (email,password,firstname,lastname) VALUES ($email, $password, $firstname, $lastname)");
+    $conn = connectDb();
+    echo "coucou";
+    $sql = "INSERT INTO person(email,password,firstname,lastname)VALUES('$email','$password','$firstname','$lastname')";
+    echo "ca marche";
+    $conn->exec($sql);
+    echo "c'est fait";
+
+
 }
+//
+////Editer l'email
+//function editEmail(&$conn, $email, $idperson)
+//{
+//    $conn->exec("UPDATE person SET email = $email WHERE idperson = $idperson");
+//}
+//
+////Editer le mot de passe
+//function editPassword(&$conn, $password, $idperson)
+//{
+//    $conn->exec("UPDATE person SET password = $password WHERE idperson = $idperson");
+//}
+//
+////Editer le prénom
+//function editFirstname(&$conn, $firstname, $idperson)
+//{
+//    $conn->exec("UPDATE person SET firstname = $firstname WHERE idperson = $idperson");
+//}
+//
+////Editer le nom
+//function editLastname(&$conn, $lastname, $idperson)
+//{
+//    $conn->exec("UPDATE person SET lastname = $lastname WHERE idperson = $idperson");
+//}
+//
+////Supprimer une personne
+//function deletePerson(&$conn, $idperson)
+//{
+//    $conn->exec("DELETE FROM person WHERE idperson = $idperson");
+//}
+//
+//
+////Rechercher une personne
+//function searchPerson(&$conn,$idperson)
+//{
+//    $conn->query("SELECT * FROM person WHERE idperson = $idperson");
+//}
+//
+//// Rechercher une personne par l'ID de son organisation
+//
+//function searchByOrganization(&$conn,$idorganization)
+//{
+//    $conn->query("SELECT * FROM person
+//                  INNER JOIN organization ON person.idperson = organization.idperson
+//                  WHERE idorganization = $idorganization");
+//}
 
-//Editer l'email
-function editEmail(&$conn, $email, $idperson)
-{
-    $conn->exec("UPDATE person SET email = $email WHERE idperson = $idperson");
-}
 
-//Editer le mot de passe
-function editPassword(&$conn, $password, $idperson)
-{
-    $conn->exec("UPDATE person SET password = $password WHERE idperson = $idperson");
-}
-
-//Editer le prénom
-function editFirstname(&$conn, $firstname, $idperson)
-{
-    $conn->exec("UPDATE person SET firstname = $firstname WHERE idperson = $idperson");
-}
-
-//Editer le nom
-function editLastname(&$conn, $lastname, $idperson)
-{
-    $conn->exec("UPDATE person SET lastname = $lastname WHERE idperson = $idperson");
-}
-
-//Supprimer une personne
-function deletePerson(&$conn, $idperson)
-{
-    $conn->exec("DELETE FROM person WHERE idperson = $idperson");
-}
+createPerson("a","b","c","d");
 
 
-//Rechercher une personne
-function searchPerson(&$conn,$idperson)
-{
-    $conn->query("SELECT * FROM person WHERE idperson = $idperson");
-}
 
-// Rechercher une personne par l'ID de son organisation
-
-function searchByOrganization(&$conn,$idorganization)
-{
-    $conn->query("SELECT * FROM person 
-                  INNER JOIN organization ON person.idperson = organization.idperson
-                  WHERE idorganization = $idorganization");
-}
