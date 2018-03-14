ec3da74049ca58f513994fe7e3e5d32722f9d657
Eleonore <eleonore.dehombreux@imie.fr>
2018-03-14 16:01:02 +0100

    Editing campaign

---
+++ Services/editOrganization.php
@@ -0,0 +1,23 @@
+<?php
+include '../Utils/generics.php';
+
+
+// Crée une campagne de vote liée à une organisation (et donc à un compte utilisateur)
+
+function editCampaign() {
+
+    $idorganization = $_POST['id'];
+    $startdate= $_POST['startdate'];
+    $enddate= $_POST['enddate'];
+    $numberoptions= $_POST['numberoptions'];
+
+
+    $name = $_POST['name'];
+    $fields =array('idorganization','name','startdate','enddate','numberoptions');
+    $data=array($idorganization,$name,$startdate,$enddate,$numberoptions);
+
+    $newid= create('campaign',$fields,$data);
+
+    return $newid;
+
+}
