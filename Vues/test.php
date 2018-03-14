7ad203200e90734f3994a2a437e496ff386242f4
Eleonore <eleonore.dehombreux@imie.fr>
2018-03-14 11:32:29 +0100

    display organization and campaign datas

--- Vues/test.php
+++ Vues/test.php
@@ -14,35 +14,75 @@ session_start();
 <body>
 
     <?php
-
-    $sql = " SELECT name FROM organization WHERE `idperson` = '6'";
+    $id = $_SESSION['idperson'];
+    $sql = " SELECT name FROM organization WHERE `idperson` = $id";
     $pdo = connectDb();
     $data = $pdo->query($sql);
 
-       ?>
+    $sql2 = " SELECT organization.name, campaign.name, startdate, enddate FROM campaign
+              INNER JOIN organization ON campaign.idorganization=organization.idorganization 
+              INNER JOIN person ON organization.idperson=person.idperson
+              WHERE person.idperson = $id
+              ORDER BY campaign.idorganization ASC";
+    $pdo = connectDb();
+    $datas = $pdo->query($sql2);
+
+
+    ?>
 
 <table>
-    <thread>
-        <tr>
-            <th>Nom de l'organisation</th>
-        </tr>
-    </thread>
-<tbody>
-<?php
-    while ($results = $data->fetch()){
+        <thread>
+            <tr>
+                <th>Nom de l'organisation</th>
+            </tr>
+        </thread>
+        <tbody>
+        <?php
+        while ($results = $data->fetch()){
 
-        ?>
-<tr>
-    <td><?php echo $results['name']?></td>
-</tr>
-<?php
-} ?>
-
-
+            ?>
+            <tr>
+                <td><?php echo $results['name']?></td>
+            </tr>
+            <?php
+        } ?>
 
 
-</tbody>
-</table>
+
+
+        </tbody>
+    </table>
+
+
+    <table>
+        <thread>
+            <tr>
+                <th>Nom de l'organisation</th>
+                <th>Nom de la campagne</th>
+                <th>Date de début</th>
+                <th>Date de fin</th>
+            </tr>
+        </thread>
+        <tbody>
+        <?php
+        while ($result = $datas->fetch()){
+
+            ?>
+            <tr>
+                <td><?php echo $result[0]?></td>
+                <td><?php echo $result[1]?></td>
+                <td><?php echo $result[2]?></td>
+                <td><?php echo $result[3]?></td>
+            </tr>
+            <?php
+        } ?>
+
+
+
+
+        </tbody>
+    </table>
+
 
 </body>
 </html>
