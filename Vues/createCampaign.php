6065725c5cbf0c64a3197beffc815f73cb2333ee
clemence-briteau <clemence.briteau@imie.fr>
2018-03-14 14:10:39 +0100

    page createCampaign.php

---
+++ Vues/createCampaign.php
@@ -0,0 +1,87 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="../src/style.css" rel="stylesheet" >
+    <link href="https://fonts.googleapis.com/css?family=Finger+Paint" rel="stylesheet">
+    <title>Créer une campagne</title>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">
+            <img src="../src/logo.png" width="200" height="100" class="d-inline-block align-top" alt="">
+        </a>
+        <h1>C-Ballot</h1>
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-0 col-sm-0 col-md-0 col-lg-3 col-xl-3"></div>
+
+            <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <h2>Créer une campagne</h2>
+                <br>
+                <form class="form-group" action="#" method="post">
+                    <label for="campaignName">Nom de la campagne :</label>
+                    <input type="text" id="campaignName" name="" class="form-control">
+
+                    <label for="startDate">Date de début :</label>
+                    <input type="date" id="startDate" class="form-control">
+
+                    <label for="endDate">Date de fin :</label>
+                    <input type="date" id="endDate" class="form-control">
+
+                    <label for="optionsNb">Nombre d'options de vote :</label>
+                    <input type="number" id="optionsNb" class="form-control">
+
+                    <label for="options">Options de vote : </label>
+                    <input type="text" id="options" class="form-control">
+                    <span><img src="../src/plus.svg" width="19" height="19" alt="Ajouter une option"> Ajouter une option</span>
+
+
+                    <label for="emails">Emails des votants :</label>
+                    <textarea id="emails" class="form-control"></textarea>
+
+                    <hr>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col"></div>
+                            <div class="col">
+                                <button type="submit" class="btn btn-light">Créer la campagne</button>
+                            </div>
+                            <div class="col"></div>
+                        </div>
+                    </div>
+
+
+                </form>
+            </div>
+            <div class="col-0 col-sm-0 col-md-0 col-lg-3 col-xl-3"></div>
+
+        </div>
+
+        <div class="row"><br></div>
+
+
+        <footer>
+            C-Ballot &#169; 2018 - Hein Team
+        </footer>
+
+    </div>
+
+
+</body>
+
+</html>
+
+
+
+
--- index.php
+++ index.php
@@ -9,23 +9,24 @@ session_start();
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="src/style.css" rel="stylesheet" >
-    <link href="https://fonts.googleapis.com/css?family=Finger+Paint|PT+Sans+Narrow" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Finger+Paint" rel="stylesheet">
     <title>C-Ballot</title>
 </head>
 
 <body>
 
     <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="index.php">
             <img src="src/logo.png" width="200" height="100" class="d-inline-block align-top" alt="">
-            <h1>C-Ballot</h1>
         </a>
+        <h1>C-Ballot</h1>
     </nav>
 
     <div class="container">
 
         <div class="row">
-            <div id="description" class="col">
+            <div class="col-0 col-sm-0 col-md-1 col-lg-1 col-xl-1"></div>
+            <div id="description" class="col-12 col-sm-12 col-md-10 col-lg-10 col-xl-10">
                 <h2>Qui sommes-nous ?</h2>
                 <p>
                     C-Ballot est une plateforme de vote en ligne qui vous permet de créer une organisation
@@ -37,30 +38,41 @@ session_start();
 
                 </p>
             </div>
+            <div class="col-0 col-sm-0 col-md-1 col-lg-1 col-xl-1"></div>
         </div>
 
         <div class="row">
 
-            <div id="login" class="col">
+            <div id="login" class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
                 <h2>Se connecter</h2>
-                <form class="form-group" action="../Services/login.php"  method="post">
+                <form class="form-group" action="Services/login.php"  method="post">
                     <label>Email :</label>
                     <input type="email" name="email" class="form-control">
 
                     <label>Mot de passe :</label>
                     <input type="password" name="mdp" class="form-control">
-                    <br>
-                    <button type="submit" class="btn btn-light">Connexion</button>
+                    <hr>
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                                <button type="submit" class="btn btn-light">Connexion</button>
+                            </div>
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+                        </div>
+                    </div>
+
                 </form>
             </div>
 
-            <div id="register" class="col">
+            <div id="register" class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
                 <h2>S'inscrire</h2>
-                <form class="form-group" action="../Services/signup.php" method="post">
+                <form class="form-group" action="Services/signup.php" method="post">
                     <label>Nom :</label>
                     <input type="text" name="lastname" class="form-control">
 
-                    <label>Prénom</label>
+                    <label>Prénom :</label>
                     <input type="text" name="firstname" class="form-control">
 
                     <label>Email :</label>
@@ -71,14 +83,24 @@ session_start();
 
                     <label>Confirmation mot de passe :</label>
                     <input type="password" name="confirmMdp" class="form-control">
-                    <br>
-                    <button type="submit" class="btn btn-light">S'inscrire</button>
+                    <hr>
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                                <button type="submit" class="btn btn-light">S'inscrire</button>
+                            </div>
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+                        </div>
+                    </div>
+
                 </form>
             </div>
+
         </div>
 
-        <div class="row">allo</div>
-
+        <div class="row"><br></div>
 
     <footer>
         C-Ballot &#169; 2018 - Hein Team
