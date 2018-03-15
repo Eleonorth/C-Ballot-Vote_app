83fbfdd7973d7cfb95173b5f13dabeaf34a264b6
clemence-briteau <clemence.briteau@imie.fr>
2018-03-15 12:26:40 +0100

    profile.php remise en forme, ajout des actions des campagnes, et bouton se déconnecter

--- Vues/profile.php
+++ Vues/profile.php
@@ -38,54 +38,71 @@ $datas = $pdo->query($sql2);
 
 ?>
 
-<nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="../index.php">
-        <img src="../src/logo.png" width="200" height="100" class="d-inline-block align-top" alt="">
-    </a>
-    <h1>C-Ballot</h1>
-</nav>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="../index.php">
+            <img src="../src/logo.png" width="200" height="100" class="d-inline-block align-top" alt="">
+        </a>
+        <h1>C-Ballot</h1>
+    </nav>
 
-<!-- div info personne + edit/delete -->
-<div class="container-fluid" id="IDinfo">
-    <div class="row">
-        <div class="col-lg-4" id="persoInfo">
-            <div class="col-lg-12">
-                <p>
-                    <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                        <img src="../src/user.png" width="256" height="256">
-                    </a>
-                </p>
-                <div class="collapse" id="collapseExample">
-                    <div class="card card-body">
-                        <button type="button" class="btn btn-primary">Modifier mon profil</button>
-                        <button type="button" class="btn btn-danger">SUPPRIMER</button>
+    <div class="container">
+
+        <!-- Ligne informations utilisateurs-->
+        <div class="row">
+
+            <div class="col" id="IDinfo">
+
+                <div class="col">
+                    <img src="../src/user.png" width="150" height="150">
+                </div>
+
+                <div class="col">
+                    <?php $user = $profile->fetch(); ?>
+                    <p> Nom : <?php echo $user['lastname'] ?> </p>
+                    <p> Prénom : <?php echo $user['firstname'] ?> </p>
+                    <p> Adresse mail : <?php echo $user['email'] ?> </p>
+                </div>
+
+                <div class="col">
+                    <p>
+                        <a class="gestion" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Gérer mes informations
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapseExample">
+                        <div class="card card-body">
+                            <button type="button" class="btn btn-primary">Modifier mon profil</button>
+                            <button type="button" class="btn btn-danger">SUPPRIMER</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-lg-2">
-            <div style="padding: auto auto; margin: auto auto;">
-                <?php
-                $user = $profile->fetch();
-                ?>
-                <p> Nom : <?php echo $user['lastname'] ?></p>
-                <p> Prénom : <?php echo $user['firstname'] ?> </p>
-                <p> Adresse mail : <?php echo $user['email'] ?></p>
-            </div>
-        </div>
-        <div class="col-lg-6">
-        </div>
-    </div>
 
-    <div class="container-fluid" id="IDorga">
+                <div class="col">
+                    <button class="btn btn-light" style="float: right">
+                        <img src="../src/logout.svg" width="20" height="20">
+                        <span class="gestion">Se déconnecter</span>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+        <!-- Fin ligne informations utilisateurs-->
+
+        <div class="row"><br></div>
+
+        <!-- Div organisations -->
         <div class="row">
-            <div class="col-lg-3"><h3>Mes organisations :</h3></div>
-            <div class="col-lg-3" style="padding: 0;"><!-- Button trigger modal -->
-                <div style="float: right;">
+            <div class="col">
+                <h3>Mes organisations :</h3>
+            </div>
+
+            <div class="col"></div>
+
+            <div class="col">
+                <div style="float: right">
                     <button type="button" class="btn btn-light" data-toggle="modal" data-target="#exampleModalCenter">
                         Ajouter une organisation <img src="../src/add.png" style="margin-left: 2px;">
                     </button>
-
                     <!-- Modal -->
                     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -98,7 +115,6 @@ $datas = $pdo->query($sql2);
                                 </div>
                                 <div class="modal-body">
                                     <form action="../Services/createOrganization.php" method="post">
-
                                         Entrez le nom de l'organisation à créer : <input type="text" name="name">
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
@@ -108,66 +124,27 @@ $datas = $pdo->query($sql2);
                                 </div>
                             </div>
                         </div>
-                    </div></div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-6"></div>
         </div>
+
         <div class="row">
-            <div class="col-lg-6 resTab">
-                <table>
+
+            <div class="col resTab">
+                <table class="table">
                     <thread>
                         <tr>
                             <th>Nom de l'organisation</th>
                         </tr>
                     </thread>
                     <tbody>
-                    <?php
-                    while ($results = $data->fetch()){
-
-                        ?>
+                    <?php while ($results = $data->fetch()){ ?>
                         <tr>
-                            <td><?php echo $results['name']?></td>
-                            <td><a href="test.php">Editer</a></td>
-                            <td><a href="test.php">Supprimer</a></td>
-                            <td><a href="createCampaign.php?id=<?php echo $results[1]?>">Créer une campagne</a></td>
-                        </tr>
-                        <?php
-                    } ?>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-lg-6"></div>
-        </div>
-    </div>
-
-    <div class="container-fluid" id="IDcampaign">
-        <div class="row">
-            <div class="col-lg-4"><h3>Mes campagnes :</h3></div>
-            <div class="col-lg-8"><button type="button" class="btn btn-light">
-                    Ajouter une campagne <img src="../src/add.png" style="margin-left: 2px;">
-                </button></div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 resTab">
-                <table>
-                    <thread>
-                        <tr>
-                            <th>Nom de l'organisation</th>
-                            <th>Nom de la campagne</th>
-                            <th>Date de début</th>
-                            <th>Date de fin</th>
-                        </tr>
-                    </thread>
-                    <tbody>
-                    <?php
-                    while ($result = $datas->fetch()){
-
-                        ?>
-                        <tr>
-                            <td><?php echo $result[0]?></td>
-                            <td><?php echo $result[1]?></td>
-                            <td><?php echo $result[2]?></td>
-                            <td><?php echo $result[3]?></td>
+                            <td> <?php echo $results['name']?> </td>
+                            <td> <a href="#">Editer</a> </td>
+                            <td> <a href="#">Supprimer</a> </td>
+                            <td> <a href="createCampaign.php?id=<?php echo $results[1]?>">Créer une campagne</a> </td>
                         </tr>
                         <?php
                     } ?>
@@ -175,7 +152,54 @@ $datas = $pdo->query($sql2);
                 </table>
             </div>
         </div>
+        <!-- Fin div organisations -->
+
+
+        <!-- Div campagnes -->
+        <div id="IDcampaign">
+
+            <div class="row">
+                <div class="col">
+                    <h3>Mes campagnes :</h3>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col resTab">
+                    <table class="table">
+                        <thread>
+                            <tr>
+                                <th>Nom de l'organisation</th>
+                                <th>Nom de la campagne</th>
+                                <th>Date de début</th>
+                                <th>Date de fin</th>
+                            </tr>
+                        </thread>
+                        <tbody>
+                        <?php while ($result = $datas->fetch()){ ?>
+                            <tr>
+                                <td><?php echo $result[0]?></td>
+                                <td><?php echo $result[1]?></td>
+                                <td><?php echo $result[2]?></td>
+                                <td><?php echo $result[3]?></td>
+                                <td><a href="#">Clôturer</a></td>
+                                <td><a href="">Editer</a></td>
+                                <td><a href="#">Supprimer</a></td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- Fin div campagnes -->
+
+        <div class="row"><br></div>
+        <div class="row"><br></div>
     </div>
+<!-- Fin du container -->
+
+
     <footer>
         C-Ballot &#169; 2018 - Hein Team
     </footer>
